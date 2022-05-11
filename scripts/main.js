a4ff5aecf775a67e87c75e42e6291e288b704445
ankithkanderi24 <ankithkanderi@outlook.com>
2022-05-11 11:28:07 -0400

    some changes

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+function multiply1(num1,num2) {
+    let result = num1 * num2;
+    return result;
+  }
+
+  document.querySelector('html').addEventListener('click', function() {
+    alert('Ouch! Stop poking me!');
+  });
+  
